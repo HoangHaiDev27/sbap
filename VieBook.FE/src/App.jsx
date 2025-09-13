7724cd81a277cfb8ef74ae0d0af4d3f7f7851bef
HoangHaiDev27 <hoanghai270623@gmail.com>
2025-09-13 10:10:15 +0700

    Update button role

--- VieBook.FE/src/App.jsx
+++ VieBook.FE/src/App.jsx
@@ -8,7 +8,7 @@ import AppRoutes from "./routes/AppRoutes";
 import { useLocation } from "react-router-dom";
 function App() {
   const location = useLocation();
-  const [role, setRole] = useState("staff");
+  const [role, setRole] = useState("");
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const noLayoutRoutes = ["/auth"];
   const hideLayout = noLayoutRoutes.includes(location.pathname);
@@ -64,8 +64,37 @@ function App() {
 
         {/* Footer */}
         <Footer />
+        <div className="flex gap-4 mt-4">
+  <button
+    onClick={() => setRole("staff")}
+    className="px-4 py-2 rounded-2xl bg-blue-500 text-white hover:bg-blue-600 shadow-md transition"
+  >
+    Staff
+  </button>
+  <button
+    onClick={() => setRole("admin")}
+    className="px-4 py-2 rounded-2xl bg-green-500 text-white hover:bg-green-600 shadow-md transition"
+  >
+    Admin
+  </button>
+  <button
+    onClick={() => setRole("owner")}
+    className="px-4 py-2 rounded-2xl bg-purple-500 text-white hover:bg-purple-600 shadow-md transition"
+  >
+    Owner
+  </button>
+  <button
+    onClick={() => setRole("user")}
+    className="px-4 py-2 rounded-2xl bg-gray-500 text-white hover:bg-gray-600 shadow-md transition"
+  >
+    User
+  </button>
+</div>
+
       </div>
+      
     </div>
+    
   );
 }
 
