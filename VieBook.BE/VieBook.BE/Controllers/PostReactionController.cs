9812203ac785e23d07e2d6cd34fc9901036e6836
tnhan216 <113283887+tnhan216@users.noreply.github.com>
2025-11-23 21:14:24 +0700

    update author forum

--- VieBook.BE/VieBook.BE/Controllers/PostReactionController.cs
+++ VieBook.BE/VieBook.BE/Controllers/PostReactionController.cs
@@ -17,7 +17,6 @@ namespace VieBook.BE.Controllers
             _postReactionService = postReactionService;
         }
 
-        [Authorize(Roles = "Customer")]
         [HttpGet]
         public async Task<IActionResult> GetReactions(long postId)
         {
@@ -32,7 +31,6 @@ namespace VieBook.BE.Controllers
             }
         }
 
-        [Authorize(Roles = "Customer")]
         [HttpGet("count")]
         public async Task<IActionResult> GetReactionCount(long postId)
         {
@@ -47,7 +45,6 @@ namespace VieBook.BE.Controllers
             }
         }
     
-        [Authorize(Roles = "Customer")]
         [HttpGet("user")]
         public async Task<IActionResult> GetUserReaction(long postId)
         {
@@ -66,7 +63,6 @@ namespace VieBook.BE.Controllers
             }
         }
 
-        [Authorize(Roles = "Customer")]
         [HttpPost]
         public async Task<IActionResult> ToggleReaction(long postId, [FromBody] CreatePostReactionDTO createDto)
         {
@@ -90,7 +86,6 @@ namespace VieBook.BE.Controllers
             }
         }
 
-        [Authorize(Roles = "Customer")]
         [HttpDelete]
         public async Task<IActionResult> DeleteReaction(long postId)
         {
